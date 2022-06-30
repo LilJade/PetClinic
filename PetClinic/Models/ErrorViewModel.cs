fccad03236850a5f0f065116185140a59e872146
LilJade <josue2duran@hotmail.com>
2022-06-30 14:21:31 -0600

    Agregar archivos de proyecto.

---
+++ PetClinic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetClinic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
